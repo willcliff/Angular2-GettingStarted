81dc8fd92ac80bc3b87697af3c1182648f0f8347
William Clifford <william.clifford8@gmail.com>
2017-05-05 01:12:59 +0100

    created custom filter pipe and moduleId for paths

--- app/app.module.js
+++ app/app.module.js
@@ -11,6 +11,7 @@ var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
+var product_filter_pipe_1 = require("./products/product-filter.pipe");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -18,8 +19,15 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent],
+        imports: [
+            platform_browser_1.BrowserModule,
+            forms_1.FormsModule
+        ],
+        declarations: [
+            app_component_1.AppComponent,
+            product_list_component_1.ProductListComponent,
+            product_filter_pipe_1.ProductFilterPipe
+        ],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent }  from "./app.component";
 import { ProductListComponent} from "./products/product-list.component";
+import { ProductFilterPipe } from "./products/product-filter.pipe"
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ProductListComponent ],
+  imports: [ 
+  	BrowserModule, 
+  	FormsModule 
+  ],
+  declarations: [ 
+  	AppComponent, 
+  	ProductListComponent, 
+  	ProductFilterPipe 
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
