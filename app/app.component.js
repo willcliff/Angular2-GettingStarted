f481305d5cf249baebcc19f58a3923864d83b132
William Clifford <william.clifford8@gmail.com>
2017-05-11 16:06:12 +0100

    added product service

--- app/app.component.js
+++ app/app.component.js
@@ -7,6 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var product_service_1 = require("./products/product.service");
 var AppComponent = (function () {
     function AppComponent() {
         this.pageTitle = 'Acme Product Management';
@@ -15,8 +16,9 @@ var AppComponent = (function () {
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'pm-app',
-        template: "\n\t<div><h1>{{pageTitle}}</h1>\n\t\t<pm-products></pm-products>\n\t</div>\n\t"
+        selector: "pm-app",
+        template: "\n\t<div><h1>{{pageTitle}}</h1>\n\t\t<pm-products></pm-products>\n\t</div>\n\t",
+        providers: [product_service_1.ProductService]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
-	selector: 'pm-app',
+	selector: "pm-app",
 	template: `
 	<div><h1>{{pageTitle}}</h1>
 		<pm-products></pm-products>
 	</div>
-	`
+	`,
+	providers: [ProductService]
 })
 export class AppComponent {
 	pageTitle: string = 'Acme Product Management';
