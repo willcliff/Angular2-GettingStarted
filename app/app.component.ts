957a188854e34639bbe6894a9ef4742d2a259691
William Clifford <william.clifford8@gmail.com>
2017-05-12 15:09:46 +0100

    finished basic routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,11 +9,14 @@ import { ProductService } from './products/product.service';
 			<div class="container-fluid">
 				<a class="navbar-brand">{{pageTitle}}</a>
 				<ul class="nav navbar-nav">
-					<li><a [routerlink]="['/welcome']">Home</a></li>
+					<li><a [routerLink]="['/welcome']">Home</a></li>
 					<li><a [routerLink]="['/products']">Product List</a></li>
 				</ul>
 			</div>
 		</nav>
+		<div class="container">
+			<router-outlet></router-outlet>
+		</div>
 	</div>
 	`,
 	providers: [ProductService]
