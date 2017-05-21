c8b1ba43c3b07b1c2ea50064cf51188b27f8c492
William Clifford <william.clifford8@gmail.com>
2017-05-21 19:19:32 +0100

    added product module and refactored

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,42 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-
-import { ProductListComponent} from './products/product-list.component';
-import { ProductDetailGuard } from './products/product-gurad.service';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { StarComponent } from './shared/star.component';
+import { ProductModule } from "./products/product.module";
 
 @NgModule({
   imports: [ 
   	BrowserModule, 
-  	FormsModule,
     HttpModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'product/:id', 
-        canActivate: [ ProductDetailGuard ],
-        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
   declarations: [ 
   	AppComponent,
     WelcomeComponent,
-  	ProductListComponent, 
-  	ProductFilterPipe,
-  	StarComponent,
-    ProductDetailComponent
   ],
-  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
