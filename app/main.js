5d824a60704885eda1823a034c2c986b2c59fce2
William Clifford <william.clifford8@gmail.com>
2017-05-21 22:55:16 +0100

    cleanup and final edits

--- app/main.js
+++ app/main.js
@@ -4,4 +4,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+// for AoT compilation
+/*
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNfFactory } from './app.module.ngfactory';
+
+platformBrowser().bootstrapModule(AppModuleNgFactory);
+*/ 
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -3,3 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// for AoT compilation
+/*
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNfFactory } from './app.module.ngfactory';
+
+platformBrowser().bootstrapModule(AppModuleNgFactory);
+*/
